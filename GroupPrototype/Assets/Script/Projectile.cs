93b9b5d9b61234efc52b97e2e8c9fc463d50fd1e
jc <jcorkery@umich.edu>
2015-02-21 13:34:57 -0500

    Added projectile prefab and util color stuff

---
+++ GroupPrototype/Assets/Script/Projectile.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+/* A generic projectile. Based off of Enemy script. */
+public class Projectile : MonoBehaviour {
+
+	
+	//Enemy Stats Vars
+	//public GameCamera target;
+	public float speed;
+	public bool goTowardsCrystal = true;
+	//for Camera
+	public float max_dist = 6.4f;	
+
+	//public GameObject tree;
+	
+
+
+	
+	
+	//For Movement
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 target;
+	private ColorType color;
+
+	enemy_physics physics;
+
+
+
+
+
+
+
+
+	// Sets a new position for the projectile to head towards
+	public void goTo(Vector2 t) {
+		target = t;
+	}
+
+
+
+
+
+
+
+	
+	
+
+
+
+	void Start () {
+		
+		physics = GetComponent<enemy_physics>();
+		color = Util.randomColor();
+		GetComponent<MeshRenderer>().material.color = Util.getColorObject(color);
+
+
+		if (goTowardsCrystal) {
+			goTo(GameObject.FindGameObjectWithTag("Crystal").transform.position);
+		}
+
+	}
+
+	void Update () {
+		Vector2 amountToMove;
+
+		
+		
+		// Handle movement
+		amountToMove.x = Mathf.Cos (Util.getAngleVector(target, transform.position)) * speed;
+		amountToMove.y = Mathf.Sin (Util.getAngleVector(target, transform.position)) * speed;                            	
+		
+
+		
+		physics.Move(amountToMove*Time.deltaTime);
+		
+	}
+	
+	void OnTriggerEnter(Collider c) {
+
+	}
+
+
+
+
+
+
+	
+
+}
--- GroupPrototype/Assets/Script/Util.cs
+++ GroupPrototype/Assets/Script/Util.cs
@@ -1,6 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum ColorType {
+	Red,
+	Orange,
+	Yellow,
+	Green,
+	Blue,
+	Purple,
+	NumColors
+};
+
 
 // Class for generic utility functions
 public class Util : MonoBehaviour {
@@ -22,5 +32,38 @@ public class Util : MonoBehaviour {
 		Debug.DrawLine (Vector3.zero, a.normalized);
 		Debug.DrawLine (Vector3.zero, new Vector3(1, 0, 0));
 		return angle;
+	}
+
+
+	// Returns a random color.
+	static public ColorType randomColor() {
+		int colorChance = (int)Random.Range(0, (int) ColorType.NumColors);
+		switch(colorChance) {
+
+		case ((int)ColorType.Red): 		return ColorType.Red; break;
+		case ((int)ColorType.Orange): 	return ColorType.Orange; break;
+		case ((int)ColorType.Yellow): 	return ColorType.Yellow; break;
+		case ((int)ColorType.Green): 	return ColorType.Green; break;
+		case ((int)ColorType.Blue):		return ColorType.Blue; break;
+		case ((int)ColorType.Purple): 	return ColorType.Purple; break;
+
+		}
+
+		return ColorType.NumColors;
+	}
+
+	// Returns the corresponding color instance given the color enum.
+	static public Color getColorObject(ColorType c) {
+		switch(c) {
+		case ColorType.Red: 	return new Color(1, 0, 0, 1); break;
+		case ColorType.Orange: 	return new Color(1, .5f, 0, 1); break;
+		case ColorType.Yellow:	return new Color(1, 1, 0, 1); break;
+		case ColorType.Green:	return new Color(0, 1, 0, 1); break;
+		case ColorType.Blue:	return new Color(0, 0, 1, 1); break;
+		case ColorType.Purple:	return new Color(.5f, 0, 1, 1); break;
+		
+
+		}
+		return new Color(1, 1, 1, 1);
 	}
 }
